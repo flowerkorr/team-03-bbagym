326ff1a40cb900b940b4d8fff173a50d7458d5a2
minq9 <mingyu340@gmail.com>
2023-08-24 16:39:05 +0900

    회원가입 버튼 활성화 최적화

--- pmg/joinInform.js
+++ pmg/joinInform.js
@@ -1,37 +1,3 @@
-var $inputs = $(".inform");
-var $must = $(".must");
-var $plus = $(".plus");
-var $contain = $(".business-container");
-var $common = $(".common");
-var $regist = $(".regist");
-var $btn = $(".join-btn");
-
-$regist.on('click', function(){
-    $contain.css('display', 'block');
-});
-
-$common.on('click', function(){
-    $contain.css('display', 'none');
-    $plus.val('');
-});
-
-// $inputs.on('keyup', function(){
-//     if($must.val() == ''){
-//         if($common.is(":checked")){
-//             $btn.prop('disabled', false);
-//         }
-//         if($regist.is(":checked")){
-//             if($plus.val() == ''){
-//                 $btn.prop('disabled', true);
-//             }else{
-//                 $btn.prop('disabled', false);
-//             }
-//         }
-//     }else{
-//         $btn.prop('disabled', true);
-//     }
-// });
-
 var inputId = document.querySelector('#id');
 var inputPw = document.querySelector('#pw');
 var checkPw = document.querySelector('#pw-check');
@@ -54,37 +20,49 @@ radioRegist.addEventListener('checked', activeEvent);
 inputBusiness.addEventListener('keyup', activeEvent);
 // findIdBtn.addEventListener('click', errorEvent);
 
-// var business = document.getElementById("business-container");
-// function hide(){
-//     business.classList.remove("show");
-// }
-// function show(){
-//     business.classList.add("show");
-// }
-
-function activeEvent() {
-    if (radioRegist.value) {
-        if (inputId.value &&
+function activeEvent(){
+    if(radioRegist.value){
+        if(inputId.value &&
             inputPw.value &&
             checkPw.value &&
             inputName.value &&
             inputPhone.value &&
             inputEmail.value &&
-            joinBtn.value) {
+            inputBusiness.value) {
             joinBtn.disabled = false;
-        } else {
+        }else{
             joinBtn.disabled = true;
         }
-    } else {
-        if (inputId.value &&
+    }
+    if(radioCommon.value) {
+        if(inputId.value &&
             inputPw.value &&
             checkPw.value &&
             inputName.value &&
             inputPhone.value &&
             inputEmail.value) {
             joinBtn.disabled = false;
-        } else {
+        }else{
             joinBtn.disabled = true;
         }
     }
 }
+
+var $inputs = $(".inform");
+var $must = $(".must");
+var $plus = $(".plus");
+var $contain = $(".business-container");
+var $common = $(".common");
+var $regist = $(".regist");
+var $btn = $(".join-btn");
+
+$regist.on('click', function(){
+    $contain.css('display', 'block');
+    $btn.prop('disabled', true);
+    $plus.val('');
+});
+
+$common.on('click', function(){
+    $contain.css('display', 'none');
+    $plus.val('');
+});
